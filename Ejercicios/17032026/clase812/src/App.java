20d703943e849a6986b9d295cde9b967bf282700
Prosachi <syosa@cesde.net>
2026-03-24 20:23:13 -0500

    semana 8

---
+++ Ejercicios/17032026/clase812/src/App.java
@@ -0,0 +1,17 @@
+import model.Cuenta;
+import model.CuentaAhorro;
+import model.CuentaCorriente;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Cuenta juan = new CuentaAhorro();
+        CuentaAhorro pedro = new CuentaAhorro();
+        CuentaCorriente luis = new CuentaCorriente();
+
+        pedro.consultarSaldo();
+        luis.consultarSaldo();
+        juan.consultarSaldo();
+
+    }
+}
